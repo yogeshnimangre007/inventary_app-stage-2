dd5f5dabd386bc86fa5a5e1bdef9696ff29cd46e
yog07 <yogeshnimangre007@gmail.com>
2018-10-16 18:36:20 +0530

    first submite

---
+++ README.md
@@ -0,0 +1,24 @@
+# Inventory-App-Stage-2
+My ninth and the Last :) project at Misk-Udacity - Android Basics by Google Nanodegree Program
+
+<img src="https://github.com/Muneera-Salah/Inventory-App-Stage-2/blob/master/screenshots/1.png" height="600">
+
+<img src="https://github.com/Muneera-Salah/Inventory-App-Stage-2/blob/master/screenshots/2.png" height="600">
+
+<img src="https://github.com/Muneera-Salah/Inventory-App-Stage-2/blob/master/screenshots/3.png" height="600">
+
+<img src="https://github.com/Muneera-Salah/Inventory-App-Stage-2/blob/master/screenshots/4.png" height="600">
+
+<img src="https://github.com/Muneera-Salah/Inventory-App-Stage-2/blob/master/screenshots/5.png" height="600">
+
+<img src="https://github.com/Muneera-Salah/Inventory-App-Stage-2/blob/master/screenshots/6.png" height="600">
+
+<img src="https://github.com/Muneera-Salah/Inventory-App-Stage-2/blob/master/screenshots/7.png" height="600">
+
+<img src="https://github.com/Muneera-Salah/Inventory-App-Stage-2/blob/master/screenshots/8.png" height="600">
+
+<img src="https://github.com/Muneera-Salah/Inventory-App-Stage-2/blob/master/screenshots/9.png" height="600">
+
+<img src="https://github.com/Muneera-Salah/Inventory-App-Stage-2/blob/master/screenshots/10.png" height="600">
+
+<img src="https://github.com/Muneera-Salah/Inventory-App-Stage-2/blob/master/screenshots/Screenshot_2018-02-21-23-19-42.png" height="600">
---
+++ app/src/main/java/com/example/anrdoid/inventoryappstage2/data/InventoryContract.java
@@ -0,0 +1,53 @@
+package com.example.anrdoid.inventoryappstage2.data;
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+
+public class InventoryContract {
+
+    public static final String CONTENT_AUTHORITY =  "com.example.anrdoid.inventoryappstage2";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_INVENTORY = "product";
+
+    //constructor
+    public InventoryContract() {
+    }
+
+    public final static class InventoryEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_INVENTORY);
+
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" +  PATH_INVENTORY;
+
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_INVENTORY;
+
+        //Name of table DB
+        public final static String TABLE_NAME = "product";
+
+        //Column of table
+        public final static String _ID = BaseColumns._ID;
+        public final static String COLUMN_PRODUCT_NAME = "product_name";
+        public final static String COLUMN_PRODUCT_PRICE = "price";
+        public final static String COLUMN_PRODUCT_QUANTITY = "quantity";
+        public final static String COLUMN_PRODUCT_SUPPLIER_NAME = "supplier_name";
+        public final static String COLUMN_PRODUCT_SUPPLIER_PHONE_NUMBER = "supplier_phone_number";
+
+        // SUPPLIER_NAME LIST VALUES
+        public final static int SUPPLIER_UNKNOWN = 0;
+        public final static int SUPPLIER_AMAZON = 1;
+        public final static int SUPPLIER_JARIRR = 2;
+        public final static int SUPPLIER_OBEIKAN = 3;
+
+        public static boolean isValidSupplierName(int suppliername) {
+            if (suppliername == SUPPLIER_UNKNOWN || suppliername == SUPPLIER_AMAZON || suppliername == SUPPLIER_JARIRR || suppliername == SUPPLIER_OBEIKAN) {
+                return true;
+            }
+            return false;
+        }
+    }
+}
